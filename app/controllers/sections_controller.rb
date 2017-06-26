afe4ec2a6663a7edcce8a17973db63613a6add32
David Schuster <searay330@imageknight.com>
2017-06-25 21:06:23 -0400

    can now create edit and delete sections.

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -15,11 +15,22 @@ class SectionsController < ApplicationController
       flash[:error] = "Permission denied."
       redirect_to(course_assessments_path) && return
     end
+
     data = params["sections"]
-    if(Sections.where("course_id = ? && name = ?", @course.id, data["name"]).exists?)
-    	 flash[:error] = "not implemented"
-    	 redirect_to(course_sections_path) && return
+    # see if we want to delete the record
+  	if(params["commit"] == "Delete")
+  		delete
+  		return
+  	end
+
+  	# see if we want to edit an existing record
+    @old_section = Sections.where("course_id = ? && name = ?", @course.id, data["name"])
+    if(@old_section.exists?)
+    	edit
+    	return
     end
+
+    # we must want to create one then so do it
     @new_section = Sections.new
     @new_section.name = data["name"]
     @new_section.course_id = @course.id
@@ -27,15 +38,29 @@ class SectionsController < ApplicationController
     @new_section.end = @@days_lookup[data["day"]] + " " + data["end time"] + ":00"
     @new_section.save
 
-
-
-
-
     flash[:success] = "saved"
     redirect_to(course_sections_path) && return
 	end
 
 	def manage
+	end
+
+	def edit
+		data = params["sections"]
+		@old_section = @old_section.first
+		@old_section.start = @@days_lookup[data["day"]] + " " + data["start time"] + ":00"
+   		@old_section.end = @@days_lookup[data["day"]] + " " + data["end time"] + ":00"
+    	@old_section.save
+    	flash[:success] = "edit complete"
+    	redirect_to(course_sections_path) && return
+	end
+
+
+	def delete
+		data = params["sections"]
+		Sections.where("course_id = ? && name = ?", @course.id, data["name"]).first.destroy
+    	flash[:success] = "deletion complete"
+    	redirect_to(course_sections_path) && return
 	end
 
 end
