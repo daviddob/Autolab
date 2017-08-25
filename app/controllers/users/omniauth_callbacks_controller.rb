c58d0d376683e2e44ad32d0721aa51afce01ae33
daviddob <daviddob@buffalo.edu>
2017-08-25 19:30:54 -0400

    Updated person number login to add on new and existing users

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -74,7 +74,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
         # Skip sign up for CMU Shibboleth user
         data = request.env["omniauth.auth"]
         @user = User.where(email: data["uid"]+"@buffalo.edu").first # email is uid in our case
-
         # If user doesn't exist, create one first
         if @user.nil?
           @user = User.new
@@ -107,6 +106,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
           @user.password_confirmation = temp_pass
           @user.skip_confirmation!
 
+        end
+
+				if @user.email.include? "@buffalo.edu"
+					@user.first_name = data["info"]["name"]
+          @user.last_name = data["info"]["last_name"]
+          @user.person_number = data["info"]["person_number"]
         end
 
         @user.authentications.new(provider: "CMU-Shibboleth",
