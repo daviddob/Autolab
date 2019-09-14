f6e3924cb11d7b3ab11f058b7f9516205f852aa4
David Schuster <davidstech99@gmail.com>
2019-09-14 15:46:22 -0400

    removed FB

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,29 +1,4 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def facebook
-    if user_signed_in?
-      if data = request.env["omniauth.auth"]
-        # add this authentication object to current user
-        if current_user.authentications.where(provider: data["provider"],
-                                              uid: data["uid"]).empty?
-          current_user.authentications.create(provider: data["provider"],
-                                              uid: data["uid"])
-        end
-      end
-      redirect_to(root_path) && return
-    else
-      @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
-
-      if @user
-        sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
-        set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
-        return
-      else
-        # automatic cleanup of devise.* after sign in
-        session["devise.facebook_data"] = request.env["omniauth.auth"].except("extra")
-        redirect_to(new_user_registration_url) && return
-      end
-    end
-  end
 
   def google_oauth2
     if user_signed_in?
